9ce82d8192d14e5c523716218d4f8770c5eeb96d
Johnydavid <dropin2johny@gmail.com>
2023-08-25 17:27:51 +0530

    c

--- redux_project/src/components/TaskList.jsx
+++ redux_project/src/components/TaskList.jsx
@@ -5,70 +5,63 @@ import { useSelector, useDispatch } from "react-redux";
 import { setSelectedTask, removeTaskFromList } from "./slices/taskSlice";
 
 const TaskList = () => {
-
-const {taskList} = useSelector((state)=> state.tasks)
-const dispatch = useDispatch();
+  const { taskList } = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
 
   const [modalShow, setModalShow] = useState(false);
 
   const updateTask = (task) => {
     console.log("update Task");
     setModalShow(true);
-    dispatch(setSelectedTask(task))
-
+    dispatch(setSelectedTask(task));
   };
 
   const deleteTask = (task) => {
     console.log("Delete Task");
-    dispatch(removeTaskFromList(task))
+    dispatch(removeTaskFromList(task));
   };
 
   return (
     <>
-      <div >
-     
+      <div>
         <table className=" my-3 container table table-striped ">
           <thead>
             <tr className="text-center">
-            <th scope="col"> #</th>
+              <th scope="col"> #</th>
               <th scope="col"> Title</th>
               <th scope="col"> Description</th>
               <th scope="col">Actions</th>
             </tr>
           </thead>
-          <tbody >
-            {
-              taskList && taskList.map((task, index)=> {
-                return(
+          <tbody>
+            {taskList &&
+              taskList.map((task, index) => {
+                return (
                   <tr className="text-center" key={task.id}>
-                  <td>{index + 1}</td>
-                  <td>{task.title}</td>
-                  <td>{task.description}</td>
-                  <td>
-                    <button
-                      type="button"
-                      className="btn btn-primary mx-3"
-                      onClick={() => updateTask(task)}
-                    >
-                      {" "}
-                      <i className="bi bi-pencil-square"></i>
-                    </button>
-    
-                    <button
-                      type="button"
-                      className="btn btn-primary"
-                      onClick={() => deleteTask(task)}
-                    >
-                      <i className="bi bi-trash3"></i>
-                    </button>
-                  </td>
-                </tr>
+                    <td>{index + 1}</td>
+                    <td>{task.title}</td>
+                    <td>{task.description}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-primary mx-3"
+                        onClick={() => updateTask(task)}
+                      >
+                        {" "}
+                        <i className="bi bi-pencil-square"></i>
+                      </button>
 
-                )
-              })
-            }
-           
-          
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={() => deleteTask(task)}
+                      >
+                        <i className="bi bi-trash3"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
