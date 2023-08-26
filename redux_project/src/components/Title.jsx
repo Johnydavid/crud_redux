5479b5358feebdfef8973a3ab85d00779a983e30
Johnydavid <dropin2johny@gmail.com>
2023-08-26 14:01:18 +0530

    c

--- redux_project/src/components/Title.jsx
+++ redux_project/src/components/Title.jsx
@@ -5,7 +5,7 @@ const Title = () => {
   const {taskList} = useSelector((state)=> state.tasks)
   return (
     <div>
-<h1 className= {"my-3 text-center text-primary"}>Project Management</h1>
+<h1 className= {"my-3 text-center text-primary"}>Task Management</h1>
 <h2 className = {"my-3 text-center lead"}>{`Currently ${taskList.length} task(s) pending`}</h2>
         </div>
   )
