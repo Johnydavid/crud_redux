224afd806cc9b898e3d0010f8926d4ce1b25e7af
Johnydavid <dropin2johny@gmail.com>
2023-08-26 13:56:52 +0530

    c

--- redux_project/src/components/AddTask.jsx
+++ redux_project/src/components/AddTask.jsx
@@ -1,45 +1,57 @@
-import React, { useState } from 'react'
-import { addTaskToList } from './slices/taskSlice';
-import { useDispatch } from 'react-redux';
-
-
-
-
+import React, { useState } from "react";
+import { addTaskToList } from "./slices/taskSlice";
+import { useDispatch } from "react-redux";
 
 const AddTask = () => {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
- 
   const [title, setTitle] = useState(" ");
   const [description, setDescription] = useState(" ");
 
-  const addTask = (e)=>{
+  const addTask = (e) => {
     e.preventDefault();
-    console.log({title, description});
-    dispatch(addTaskToList({title, description}))
-    setTitle('')
-    setDescription('')
-
-  }
+    console.log({ title, description });
+    dispatch(addTaskToList({ title, description }));
+    setTitle("");
+    setDescription("");
+  };
   return (
     <>
-    
-    <section className=" container w-50  rounded">
-    <div className="mb-3">
-  <label  className="form-label">Task Title</label>
-  <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Task Title" value = {title} onChange={(e)=>setTitle(e.target.value)}/>
-</div>
-<div className="mb-2">
-  <label  className="form-label">Task Description</label>
-  <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Task Description" value = {description} onChange={(e)=>setDescription(e.target.value)} />
-</div>
-<div className='text-center'>
-    <button type="submit" className="my-2 btn btn-primary" onClick={addTask}>Add Task</button>
-  </div>
-    </section>
+      <section className=" container w-50 bg-secondary text-white rounded">
+        <div className="mb-3">
+          <label className="form-label">Task Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="Task Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="mb-2">
+          <label className="form-label">Task Description</label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Task Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="my-2 btn btn-primary"
+            onClick={addTask}
+          >
+            Add Task
+          </button>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default AddTask
+export default AddTask;
--- redux_project/src/components/UpdateTask.jsx
+++ redux_project/src/components/UpdateTask.jsx
@@ -1,33 +1,28 @@
+import Modal from "react-bootstrap/Modal";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { updateTaskInList } from "./slices/taskSlice";
+import { useDispatch } from "react-redux";
 
-import Modal from 'react-bootstrap/Modal';
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { updateTaskInList } from './slices/taskSlice';
-import { useDispatch } from 'react-redux';
-
-const MyVerticallyCenteredModal=(props)=> {
-  const{selectedTask} = useSelector((state)=> state.tasks)
+const MyVerticallyCenteredModal = (props) => {
+  const { selectedTask } = useSelector((state) => state.tasks);
   const [title, setTitle] = useState(" ");
   const [description, setDescription] = useState(" ");
-  const[id, setId] = useState(0)
+  const [id, setId] = useState(0);
   const dispatch = useDispatch();
 
+  const updateTask = () => {
+    props.onHide();
+    dispatch(updateTaskInList({ id, title, description }));
+  };
 
-
-    const updateTask = ()=>{
-        props.onHide()
-        dispatch(updateTaskInList({id, title, description}))
+  useEffect(() => {
+    if (Object.keys(selectedTask).length !== 0) {
+      setTitle(selectedTask.title);
+      setDescription(selectedTask.description);
+      setId(selectedTask.id);
     }
-
-    useEffect(()=>{
-      if(Object.keys(selectedTask).length !==0){
-
-      
-      setTitle(selectedTask.title)
-      setDescription(selectedTask.description)
-      setId(selectedTask.id)
-    }
-    }, [selectedTask])
+  }, [selectedTask]);
   return (
     <Modal
       {...props}
@@ -37,49 +32,47 @@ const MyVerticallyCenteredModal=(props)=> {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-       Update Task
+          Update Task
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      <section className=" my-5 container w-50">
-    <div class="mb-3">
-  <label  class="form-label">Task Title</label>
-  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Task Title" value = {title} onChange={(e)=>setTitle(e.target.value)}/>
-</div>
-<div class="mb-3">
-  <label  class="form-label">Task Description</label>
-  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Task Description" value = {description} onChange={(e)=>setDescription(e.target.value)} />
-</div>
-
-    </section>
-        
+        <section className=" my-5 container w-50">
+          <div class="mb-3">
+            <label class="form-label">Task Title</label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput"
+              placeholder="Task Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Task Description</label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput2"
+              placeholder="Task Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+        </section>
       </Modal.Body>
       <Modal.Footer>
-     
         {/* <Button onClick={props.onHide}>Close</Button> */}
-        <div className='ms-5'>
-    <button type="submit" class="btn btn-primary" onClick={updateTask}>Update Task</button>
-  </div>
+        <div className="ms-5">
+          <button type="submit" class="btn btn-primary" onClick={updateTask}>
+            Update Task
+          </button>
+        </div>
       </Modal.Footer>
     </Modal>
   );
-}
+};
 
-// function App() {
-//   const [modalShow, setModalShow] = React.useState(false);
 
-//   return (
-//     <>
-//       <Button variant="primary" onClick={() => setModalShow(true)}>
-//         Launch vertically centered modal
-//       </Button>
 
-//       <MyVerticallyCenteredModal
-//         show={modalShow}
-//         onHide={() => setModalShow(false)}
-//       />
-//     </>
-//   );
-// }
-
-export default MyVerticallyCenteredModal
+export default MyVerticallyCenteredModal;
